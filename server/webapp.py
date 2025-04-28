769a3559eb7ac17f981b1b862bc9b1f3986e8209
Glenn Rosedom <13999846+Glenn-MS@users.noreply.github.com>
2025-04-28 14:12:56 +0200

    Update webapp.py

--- server/webapp.py
+++ server/webapp.py
@@ -14,3 +14,8 @@ database_uri = os.environ.get('SQLITE_URI', ':memory:')
 
 database = sqlite3.connect(database_uri, check_same_thread=False)
 cursor = database.cursor()
+
+
+# Unsafe usage of format()
+user_input = "{0.__class__}".format("string")  # This can expose internal details
+print(user_input)
